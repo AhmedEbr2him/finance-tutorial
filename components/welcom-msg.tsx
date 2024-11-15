6d69a654da5005cc094b7ac0eb32976583a1f98f
AhmedEbrahim <89828478+AhmedEbr2him@users.noreply.github.com>
2024-11-15 18:50:36 +0200

    finish header component

---
+++ components/welcom-msg.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+
+export const WelcomMsg = () => {
+	const { user, isLoaded } = useUser();
+	return (
+		<div className='space-y-2 mb-4'>
+			<h2 className='text-2xl lg:text-4xl text-white font-medium'>
+				Welcom Back{isLoaded ? ', ' : ' '}
+				{user?.firstName}👋🏻
+			</h2>
+			<p className='text-sm lg:text-base text-[#89b6fd]'>
+				This Is Your Financial Overview Report
+			</p>
+		</div>
+	);
+};

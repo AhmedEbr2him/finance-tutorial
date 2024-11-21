908214b6628ab02aa15b01cd7889ef8af3dd0b0b
AhmedEbrahim <89828478+AhmedEbr2him@users.noreply.github.com>
2024-11-22 00:55:38 +0200

    finish dashboard pie

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useGetSummary } from '@/features/summary/api/use-get-summary';
-import { Chart } from './chart';
+import { Chart, ChartLoading } from '@/components/chart';
+import { SpendingPie, SpendingPieLoading } from '@/components/spending-pie';
 
 
 export const DataCharts = () => {
@@ -9,15 +10,25 @@ export const DataCharts = () => {
 
   if (summaryIsLoading) {
     return (
-      <div className="">
-        Loading...
+      <div>
+        <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+          <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+            <ChartLoading />
+          </div>
+          <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+            <SpendingPieLoading />
+          </div>
+        </div>
       </div>
     )
   }
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="col-span-1 lg:col-span-1-3 xl:col-span-4">
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
         <Chart data={summary?.days} />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPie data={summary?.categories} />
       </div>
     </div>
   )
--- components/spending-pie.tsx
+++ components/spending-pie.tsx
@@ -13,29 +13,29 @@ import {
   CardTitle,
   CardContent,
 } from "@/components/ui/card";
-import { Skeleton } from '@/components/ui/skeleton';
+import { Skeleton } from './ui/skeleton';
 
 import {
-  AreaChartIcon,
-  BarChart3Icon,
   FileSearchIcon,
-  LineChartIcon,
-  Loader2
+  Loader2,
+  PieChartIcon,
+  RadarIcon,
+  TargetIcon,
 } from 'lucide-react';
 
-import { AreaVariant } from '@/components/aria-variant';
-import { BarVariant } from '@/components/bar-variant';
-import { LineVariant } from '@/components/line-variant';
+import { PieVariant } from '@/components/pie-variant';
+import { RadarVariant } from '@/components/radar-variant';
+import { RadialVariant } from '@/components/radial-variant';
 
-type ChartProps = {
+type SpendingPieProps = {
   data?: {
-    date: string,
-    income: number,
-    expenses: number,
+    name: string,
+    value: number,
   }[];
-}
-export const Chart = ({ data = [] }: ChartProps) => {
-  const [chartType, setChartType] = useState("area");
+};
+
+export const SpendingPie = ({ data = [] }: SpendingPieProps) => {
+  const [chartType, setChartType] = useState("pie");
 
   const onTypeChange = (type: string) => {
     // TODO: ADD PAYWALL
@@ -46,7 +46,7 @@ export const Chart = ({ data = [] }: ChartProps) => {
     <Card className="border-none drop-shadow-sm">
       <CardHeader className='flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between'>
         <CardTitle className='text-xl line-clamp-1'>
-          Transactions
+          Categories
         </CardTitle>
 
         <Select
@@ -58,22 +58,22 @@ export const Chart = ({ data = [] }: ChartProps) => {
           </SelectTrigger>
 
           <SelectContent>
-            <SelectItem value='area'>
+            <SelectItem value='pie'>
               <div className="flex items-center">
-                <AreaChartIcon className='size-4 mr-2 shrink-0' />
-                <p className="line-clamp-1">Area Chart</p>
+                <PieChartIcon className='size-4 mr-2 shrink-0' />
+                <p className="line-clamp-1">Pie Chart</p>
               </div>
             </SelectItem>
-            <SelectItem value='line'>
+            <SelectItem value='radar'>
               <div className="flex items-center">
-                <LineChartIcon className='size-4 mr-2 shrink-0' />
-                <p className="line-clamp-1">Line Chart</p>
+                <RadarIcon className='size-4 mr-2 shrink-0' />
+                <p className="line-clamp-1">Radar Chart</p>
               </div>
             </SelectItem>
-            <SelectItem value='bar'>
+            <SelectItem value='radial'>
               <div className="flex items-center">
-                <BarChart3Icon className='size-4 mr-2 shrink-0' />
-                <p className="line-clamp-1">Bar Chart</p>
+                <TargetIcon className='size-4 mr-2 shrink-0' />
+                <p className="line-clamp-1">Radial Chart</p>
               </div>
             </SelectItem>
           </SelectContent>
@@ -94,17 +94,18 @@ export const Chart = ({ data = [] }: ChartProps) => {
             :
             (
               <>
-                {chartType === "area" && <AreaVariant data={data} />}
-                {chartType === "line" && <LineVariant data={data} />}
-                {chartType === "bar" && <BarVariant data={data} />}
+                {chartType === "pie" && <PieVariant data={data} />}
+                {chartType === "radar" && <RadarVariant data={data} />}
+                {chartType === "radial" && <RadialVariant data={data} />}
               </>
             )
         }
       </CardContent>
-    </Card>)
+    </Card>
+  )
 };
 
-export const ChartLoading = () => {
+export const SpendingPieLoading = () => {
   return (
     <Card className='border-none drop-shadow-sm'>
       <CardHeader className='flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between'>
@@ -119,4 +120,4 @@ export const ChartLoading = () => {
       </CardContent>
     </Card>
   )
-}
+};
